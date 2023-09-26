23103ff40e68f69ff9d5b5821188af65f781613b
Johann Schopplich <mail@johannschopplich.com>
2023-09-26 13:23:11 +0200

    feat: custom file resolver for block fields

--- src/extensions/fieldMethods.php
+++ src/extensions/fieldMethods.php
@@ -1,64 +1,67 @@
 <?php
 
-$filesResolver = function (\Kirby\Cms\Block $item) {
-    $kirby = $item->kirby();
-    $keys = array_values($kirby->option('blocksResolver.files', ['image' => 'image']));
+$filesFieldResolver = function (\Kirby\Cms\Block $block) {
+    $kirby = $block->kirby();
+    $blocks = $kirby->option('blocksResolver.files', ['image' => 'image']);
+
+    if (!isset($blocks[$block->type()])) {
+        return $block;
+    }
 
     // Flatten keys, since the option values can be arrays
     $keys = array_reduce(
-        $keys,
+        $blocks[$block->type()],
         fn ($acc, $i) => array_merge($acc, is_array($i) ? $i : [$i]),
         []
     );
 
-    // Get the resolver method
-    $resolver = $kirby->option('blocksResolver.resolvers.files', fn (\Kirby\Cms\File $image) => [
+    // Get the resolvers config
+    $resolvers = $kirby->option('blocksResolver.resolvers.files');
+    $defaultResolver = fn (\Kirby\Cms\File $image) => [
         'url' => $image->url(),
         'width' => $image->width(),
         'height' => $image->height(),
         'srcset' => $image->srcset(),
         'alt' => $image->alt()->value()
-    ]);
+    ];
 
     foreach ($keys as $key) {
         /** @var \Kirby\Cms\Files $images */
-        $images = $item->content()->get($key)->toFiles();
+        $images = $block->content()->get($key)->toFiles();
 
         if ($images->count() === 0) {
             continue;
         }
 
         // Get already resolved images
-        $resolved = $item->content()->get('resolved')->or([])->value();
+        $resolved = $block->content()->get('resolved')->or([])->value();
+
+        // Get specific resolver for the current block and key or fallback to default
+        $resolver = is_callable($resolvers)
+            ? $resolvers
+            : $resolvers[$block->type()][$key] ?? $resolvers['_default'] ?? $defaultResolver;
 
         // Replace the image field with the resolved image
-        $item->content()->update([
+        $block->content()->update([
             'resolved' => array_merge($resolved, [
                 strtolower($key) => $images->map($resolver)->values()
             ])
         ]);
     }
 
-    return $item;
-};
-
-$filesFieldResolver = function (\Kirby\Cms\Block $block) use ($filesResolver) {
-    $kirby = $block->kirby();
-    $resolvers = $kirby->option('blocksResolver.files', ['image' => 'image']);
-
-    if (isset($resolvers[$block->type()])) {
-        return $filesResolver($block);
-    }
-
     return $block;
 };
 
-$pagesResolver = function (\Kirby\Cms\Block $item) {
-    $kirby = $item->kirby();
-    $keys = array_values($kirby->option('blocksResolver.pages', []));
+$pagesFieldResolver = function (\Kirby\Cms\Block $block) {
+    $kirby = $block->kirby();
+    $blocks = $kirby->option('blocksResolver.pages', []);
+
+    if (!isset($blocks[$block->type()])) {
+        return $block;
+    }
 
     $keys = array_reduce(
-        $keys,
+        $blocks[$block->type()],
         fn ($acc, $i) => array_merge($acc, is_array($i) ? $i : [$i]),
         []
     );
@@ -71,30 +74,19 @@ $pagesResolver = function (\Kirby\Cms\Block $item) {
 
     foreach ($keys as $key) {
         /** @var \Kirby\Cms\Pages $pages */
-        $pages = $item->content()->get($key)->toPages();
+        $pages = $block->content()->get($key)->toPages();
 
         if ($pages->count() === 0) {
             continue;
         }
 
-        $resolved = $item->content()->get('resolved')->or([])->value();
+        $resolved = $block->content()->get('resolved')->or([])->value();
 
-        $item->content()->update([
+        $block->content()->update([
             'resolved' => array_merge($resolved, [
                 strtolower($key) => $pages->map($resolver)->values()
             ])
         ]);
-    }
-
-    return $item;
-};
-
-$pagesFieldResolver = function (\Kirby\Cms\Block $block) use ($pagesResolver) {
-    $kirby = $block->kirby();
-    $resolvers = $kirby->option('blocksResolver.pages', []);
-
-    if (isset($resolvers[$block->type()])) {
-        return $pagesResolver($block);
     }
 
     return $block;
